56c93710b3b48add9384a3bc540d97043c8f3cf4
JeffBigg <soriaarandajeferson@gmail.com>
2023-08-17 10:46:36 -0500

    hago la suma

--- src/projectojava/Projectojava.java
+++ src/projectojava/Projectojava.java
@@ -6,16 +6,24 @@ import java.util.Scanner;
 public class Projectojava {
 
     public static void main(String[] args) {
-        int n1, n2, suma, resta;
+        int n1, n2, suma, resta, operacion;
         Scanner teclado = new Scanner(System.in);
         
-        System.out.print("SUMA==1 ");
-        System.out.print("RESTA==2 ");
+        System.out.println("SUMA==1 ");
+        System.out.println("RESTA==2 ");
+        System.out.println("Ingresa la opcion -> ");
+        operacion = teclado.nextInt();
         
-        System.out.print("Introdusca el primer numero: ");
-        n1 = teclado.nextInt();
-        System.out.print("Introdusca el segundo numero: ");
-        n2 = teclado.nextInt();
+        if (operacion == 1){
+            System.out.print("Introdusca el primer numero: ");
+            n1 = teclado.nextInt();
+            System.out.print("Introdusca el segundo numero: ");
+            n2 = teclado.nextInt();
+            
+            suma = n1 + n2;
+            System.out.println("la suma es "+ suma);
+        }
+        
         
     }
     
