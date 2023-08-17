5fa2edafe11220ceace54d02b20f529be63d7654
JeffBigg <soriaarandajeferson@gmail.com>
2023-08-17 10:27:32 -0500

    agrego varibles

---
+++ src/projectojava/Projectojava.java
@@ -0,0 +1,16 @@
+
+package projectojava;
+
+import java.util.Scanner;
+
+public class Projectojava {
+
+    public static void main(String[] args) {
+        int n1, n2, suma, resta;
+        Scanner teclado = new Scanner(System.in);
+        
+        
+        
+    }
+    
+}
