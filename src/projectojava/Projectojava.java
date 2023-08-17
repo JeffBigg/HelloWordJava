124beb4cd4b69618e3e589fe4ec38ee462b14713
JeffBigg <soriaarandajeferson@gmail.com>
2023-08-17 11:54:55 -0500

    agrego el bucle hasta que escoja la opcion correcta

--- src/projectojava/Projectojava.java
+++ src/projectojava/Projectojava.java
@@ -9,31 +9,36 @@ public class Projectojava {
         int n1, n2, suma, resta, operacion;
         Scanner teclado = new Scanner(System.in);
         
-        System.out.println("SUMA==1 ");
-        System.out.println("RESTA==2 ");
-        System.out.print("Ingresa la opcion -> ");
-        operacion = teclado.nextInt();
-        
-        if (operacion == 1){
-            System.out.print("Introdusca el primer numero: ");
-            n1 = teclado.nextInt();
-            System.out.print("Introdusca el segundo numero: ");
-            n2 = teclado.nextInt();
-            
-            suma = n1 + n2;
-            System.out.println("la suma es "+ suma);
-        }else if(operacion == 2){
-            System.out.print("Introdusca el primer numero: ");
-            n1 = teclado.nextInt();
-            System.out.print("Introdusca el segundo numero: ");
-            n2 = teclado.nextInt();
-            
-            resta = n1 - n2;
-            System.out.println("la resta es "+ resta);
-        }else{
-            System.out.println("ESCOJA EL NUMERO CORRECTO");
+        while(true){
+            System.out.println("SUMA==1 ");
+            System.out.println("RESTA==2 ");
+            System.out.print("Ingresa la opcion -> ");
+            operacion = teclado.nextInt();
+
+            if (operacion == 1){
+                System.out.print("Introdusca el primer numero: ");
+                n1 = teclado.nextInt();
+                System.out.print("Introdusca el segundo numero: ");
+                n2 = teclado.nextInt();
+
+                suma = n1 + n2;
+                System.out.println("la suma es "+ suma);
+                break;
+                
+            }else if(operacion == 2){
+                System.out.print("Introdusca el primer numero: ");
+                n1 = teclado.nextInt();
+                System.out.print("Introdusca el segundo numero: ");
+                n2 = teclado.nextInt();
+
+                resta = n1 - n2;
+                System.out.println("la resta es "+ resta);
+                break;
+                
+            }else{
+                System.out.println("ESCOJA EL NUMERO CORRECTO");
+            }
         }
-        
         
     }
     
