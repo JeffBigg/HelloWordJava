84b3bfcc05083cb6c33589208070e063c9541082
JeffBigg <soriaarandajeferson@gmail.com>
2023-08-17 10:33:28 -0500

    pedir datos al usuario

--- src/projectojava/Projectojava.java
+++ src/projectojava/Projectojava.java
@@ -9,7 +9,13 @@ public class Projectojava {
         int n1, n2, suma, resta;
         Scanner teclado = new Scanner(System.in);
         
+        System.out.print("SUMA==1 ");
+        System.out.print("RESTA==2 ");
         
+        System.out.print("Introdusca el primer numero: ");
+        n1 = teclado.nextInt();
+        System.out.print("Introdusca el segundo numero: ");
+        n2 = teclado.nextInt();
         
     }
     
